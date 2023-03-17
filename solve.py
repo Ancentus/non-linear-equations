135c9b9484a3c2f8987fabae4fdd23c118cdb9db
Ancentus <mancentus7@gmail.com>
2023-03-17 23:36:22 +0300

    Add bisection method

--- solve.py
+++ solve.py
@@ -3,8 +3,30 @@ import time
 def f(x):
     return x**5 + x**3 - 2*x - 5
 
-def df(x):
-    return 5*x**4 + 3*x**2 - 2
+def bisection(a, b, tol=1e-6, max_iterations=100):
+    """
+    This function implements the bisection method to solve the nonlinear equation f(x) = 0.
+    a: left endpoint of the initial interval
+    b: right endpoint of the initial interval
+    tol: tolerance for convergence
+    max_iterations: maximum number of iterations to perform
+    """
+    fa = f(a)
+    fb = f(b)
+    if fa * fb > 0:
+        return None
+    for i in range(max_iterations):
+        c = (a + b) / 2
+        fc = f(c)
+        if abs(fc) < tol:
+            return c
+        if fa * fc < 0:
+            b = c
+            fb = fc
+        else:
+            a = c
+            fa = fc
+    return None
 
 def newton_raphson(x0, tol=1e-6, max_iterations=100):
     """
@@ -16,7 +38,7 @@ def newton_raphson(x0, tol=1e-6, max_iterations=100):
     x = x0
     for i in range(max_iterations):
         fx = f(x)
-        dfx = df(x)
+        dfx = 5*x**4 + 3*x**2 - 2
         if abs(fx) < tol:
             return x
         x = x - fx / dfx
@@ -27,7 +49,18 @@ start_time = time.time()
 root = newton_raphson(x0)
 elapsed_time = time.time() - start_time
 if root is not None:
-    print("The root is:", root)
-    print("Time taken:", elapsed_time, "seconds")
+    print("The root found by Newton-Raphson is:", root)
+    print("Time taken by Newton-Raphson:", elapsed_time, "seconds")
 else:
-    print("Failed to converge.")
+    print("Newton-Raphson failed to converge.")
+
+a = 0.0
+b = 3.0
+start_time = time.time()
+root = bisection(a, b)
+elapsed_time = time.time() - start_time
+if root is not None:
+    print("The root found by bisection is:", root)
+    print("Time taken by bisection:", elapsed_time, "seconds")
+else:
+    print("Bisection failed to converge.")
